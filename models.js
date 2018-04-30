716adf1955227f3e95c240053263fd3e208a1b3b
Eric Guo <ericxguo@gmail.com>
2018-04-30 16:26:40 -0500

    got some stuff working

--- models.js
+++ models.js
@@ -1,6 +1,47 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/plantr');
 
+const Gardener = db.define('gardener', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+})
+
+const Plot = db.define('plot', {
+  size: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  }
+})
+
+const Vegetable = db.define('vegetable', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  color: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  planted_on: {
+    type: Sequelize.DATE
+  }
+})
+
+Plot.belongsTo(Gardener, { as: 'gardener' });
+Plot.belongsToMany(Vegetable, { through: 'vegetable_plot' });
+Vegetable.belongsToMany(Plot, { through: 'vegetable_plot' });
+Gardener.belongsTo(Vegetable, { as: 'favoriteVegetable' });
+
 // db.authenticate().then( () => {
 //   console.log("connected to database");
 // })
